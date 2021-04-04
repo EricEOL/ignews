23ce2978131d7c687c4ceefedb466ace02c7342e
EricEOL <lopes.eric051@gmail.com>
2021-04-04 14:02:03 -0300

    Integração com o FaunaDB

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,14 +1,37 @@
+import { useSession, signIn } from 'next-auth/client';
 import styles from './styles.module.scss';
+import { api } from '../../services/api';
 
-interface SubscribeButtonProps{
+interface SubscribeButtonProps {
     priceId: string;
 }
 
-export function SubscribeButton({priceId}: SubscribeButtonProps) {
+export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+    const [session] = useSession();
+
+    async function handleSubscribe() {
+        if (!session) {
+            signIn('github')
+            return;
+        }
+
+        try {
+            const response = await api.post('/subscribe');
+
+            const { sessionId } = response.data;
+
+            
+        } catch {
+
+        }
+
+    }
+
     return (
         <button
             type="button"
             className={styles.subscribeButton}
+            onClick={handleSubscribe}
         >
             Subscribe now
         </button>
